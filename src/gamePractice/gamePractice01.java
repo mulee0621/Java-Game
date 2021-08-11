8bd34d27505b8749e8fb34f322b12892903d5110
Myung <mulee0621@gmail.com>
2021-08-11 11:35:36 +0200

    Start to make GUI-Type Game | With Swing | Set Frame and two button | Change button location

--- src/gamePractice/gamePractice01.java
+++ src/gamePractice/gamePractice01.java
@@ -2,64 +2,44 @@
 
 package gamePractice;
 
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class gamePractice01 {
 
     public static void main(String[] args) {
     	
-    	Slime s = null;
+    	// Creat Frame
+    	JFrame frm = new JFrame("Killing Slimes");
     	
-    	// Slime and Human Object created
-    	Slime s1 = new Slime("Slammi");
-    	Slime s2 = new Slime("Sladdi");
-    	Human h = new Human("Alex");
+    	// Size of Frame
+    	frm.setSize(350,300);
     	
-    	System.out.println(s1.name + " and " + s2.name + "is created!! And your name is " + h.name + "\n");
-    
-    	while(true) {
-    		
-    		// Input Box
-    		String sTarget = JOptionPane.showInputDialog("Which Slime will you attack?");
-    		
-    		//For cancle
-    		if(sTarget == null || sTarget.equals("")) {
-    			System.exit(0);
-    		}
-    			
-    		int target = Integer.parseInt(sTarget);
-        	
-        	if(target == 1) {
-        		
-        		s = s1;
-        		
-        	} else if(target == 2) {
-        		
-        		s = s2;
-        		
-        	} else {
-        		
-        		JOptionPane.showMessageDialog(null, "Please insert proper number.");
-        	} 
-        	
-        	if(s.hp < 1) {
-        		
-        		System.out.println(s.name + "is already dead. \n");
-        	
-        	} else {
-        		
-        		h.attack(s);
-            	s.attack(h);
-    
-        	}
-        	
-        	//If all slimes are dead, it is Game Clear!
-        	if(s1.hp < 1 && s2.hp < 1) {
-        		
-        		JOptionPane.showMessageDialog(null, "***Game Clear!***");
-        		System.exit(0);
-        		
-        	}
-    	} // while END
+    	// Set Frame location in the middle
+    	frm.setLocationRelativeTo(null);
+    	
+    	// By closing Frame deleted from memory***
+    	frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	
+    	//Layout Setting
+    	frm.getContentPane().setLayout(null);
+    	
+    	// Make a button
+    	JButton btn1 = new JButton("Slappi");
+    	btn1.setBounds(30, 170, 122, 30);
+    	frm.getContentPane().add(btn1);
+    	
+    	JButton btn2 = new JButton("Sladdi");
+    	btn2.setBounds(182, 170, 122, 30);
+    	frm.getContentPane().add(btn2);
+    	
+    	// Make frame visible***
+    	frm.setVisible(true);
+    	
+    	// Set size of Content pane
+    	System.out.println(frm.getContentPane().getSize());
     }
 }
